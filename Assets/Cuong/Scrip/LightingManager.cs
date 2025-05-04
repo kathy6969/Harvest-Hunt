d71d1cdb562df35535e60355e13cc13c37cbcb93
Cuong3cdz <lop8a82003@gmail.com>
2025-05-04 21:17:49 +0700

    time
    day/night

---
+++ Assets/Cuong/Scrip/LightingManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightingManager : MonoBehaviour
+{
+    public Light2D globalLight;
+
+    // Cường độ sáng theo buổi
+    public float morningIntensity = 0.5f;
+    public float noonIntensity = 0.9f;
+    public float eveningIntensity = 0.4f;
+    public float nightIntensity = 0.1f;
+
+    public Color morningColor = new Color(1f, 0.95f, 0.8f);
+    public Color noonColor = Color.white;
+    public Color eveningColor = new Color(1f, 0.6f, 0.4f);
+    public Color nightColor = new Color(0.1f, 0.1f, 0.3f);
+
+    void OnEnable()
+    {
+        TimeManager.OnTimeChanged += UpdateLighting;
+    }
+
+    void OnDisable()
+    {
+        TimeManager.OnTimeChanged -= UpdateLighting;
+    }
+
+    void UpdateLighting(float hour)
+    {
+        float intensity = 1f;
+        Color color = Color.white;
+
+        if (hour >= 6f && hour < 12f) // Sáng
+        {
+            float t = (hour - 6f) / 6f;
+            intensity = Mathf.Lerp(morningIntensity, noonIntensity, t);
+            color = Color.Lerp(morningColor, noonColor, t);
+        }
+        else if (hour >= 12f && hour < 18f) // Chiều
+        {
+            float t = (hour - 12f) / 6f;
+            intensity = Mathf.Lerp(noonIntensity, eveningIntensity, t);
+            color = Color.Lerp(noonColor, eveningColor, t);
+        }
+        else if (hour >= 18f && hour < 24f) // Tối
+        {
+            float t = (hour - 18f) / 6f;
+            intensity = Mathf.Lerp(eveningIntensity, nightIntensity, t);
+            color = Color.Lerp(eveningColor, nightColor, t);
+        }
+        else // Đêm đến sáng
+        {
+            float t = hour / 6f;
+            intensity = Mathf.Lerp(nightIntensity, morningIntensity, t);
+            color = Color.Lerp(nightColor, morningColor, t);
+        }
+
+        globalLight.intensity = intensity;
+        globalLight.color = color;
+    }
+}
+
---
+++ Assets/Cuong/Scrip/TimeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float realSecondsPerGameDay = 15f; // 24p ngoài đời = 1 ngày game
+    public float currentTime; // thời gian hiện tại trong ngày game (0 đến 24)
+
+    public delegate void TimeChanged(float gameHour);
+    public static event TimeChanged OnTimeChanged;
+
+    void Update()
+    {
+        // Tăng thời gian theo thời gian thực
+        currentTime += (24f / realSecondsPerGameDay) * Time.deltaTime;
+
+        if (currentTime >= 24f)
+            currentTime -= 24f;
+        Debug.Log("Giờ trong game: " + currentTime);
+        OnTimeChanged?.Invoke(currentTime);
+    }
+}
+
